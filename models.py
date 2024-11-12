e7ecb51cdc2eff06555e0a72a33c954ca29c9fbb
Syed Zain ul Abideen Zaidi <zain.xaidi26@gmail.com>
2024-11-12 14:28:14 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login_manager
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Alert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True)
+    alert_type = db.Column(db.String(64))
+    source_ip = db.Column(db.String(64))
+    details = db.Column(db.String(256))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
