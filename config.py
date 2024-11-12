033edd680883a330b1aeb6904b58f8353f47e4dc
Syed Zain ul Abideen Zaidi <zain.xaidi26@gmail.com>
2024-11-12 14:46:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'f6138b2251231a2e9f7059b29dbe0d4d94fca82f62b393360c5e61a40588c6aa'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///ids.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
